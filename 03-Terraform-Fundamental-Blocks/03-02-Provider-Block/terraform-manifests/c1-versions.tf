2da7ad2568667a2ca26a45dfbf466155f33851a6
Mayur Hastak <80825112+vw-hm@users.noreply.github.com>
2021-10-06 17:06:16 +0200

    Demo

--- 03-Terraform-Fundamental-Blocks/03-02-Provider-Block/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Fundamental-Blocks/03-02-Provider-Block/terraform-manifests/c1-versions.tf
@@ -1,17 +1,18 @@
 # Terraform Block
 terraform {
-  required_version = "~> 0.14.6"
+  required_version = "~> 1.0.0"
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "~> 3.0"
-    }
+     }
   }
 }
 
 # Provider Block
 provider "aws" {
-  region = "us-east-1"  
+  region = "eu-west-1"  
   profile = "default" 
 }
 
