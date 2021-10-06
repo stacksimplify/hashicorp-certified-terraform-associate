6cb1e0582db9117fc17f2e01caf555967b03acdf
Mayur Hastak <80825112+vw-hm@users.noreply.github.com>
2021-10-06 16:21:51 +0200

    Demmo2

--- 03-Terraform-Fundamental-Blocks/03-01-Terraform-Block/terraform-manifests/terraform-block.tf
+++ 03-Terraform-Fundamental-Blocks/03-01-Terraform-Block/terraform-manifests/terraform-block.tf
@@ -1,15 +1,16 @@
 # Terraform Block
 terraform {
-  required_version = "~> 0.14.3"
+  required_version = "~> 1.0.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "~> 3.0"
-    }
+     }
+    
     random = {
-      source = "hashicorp/random"
-      version = "3.0.1"
-    }    
+       source = "hashicorp/random"
+       version = "~> 3.0"
+      }
   }
 }
 
