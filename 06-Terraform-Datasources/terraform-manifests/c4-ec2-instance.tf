33ba5234383ef51f34ff4e88e08d3d1de1ae20aa
Mayur Hastak <80825112+vw-hm@users.noreply.github.com>
2021-10-12 20:51:18 +0200

    addeD

--- 06-Terraform-Datasources/terraform-manifests/c4-ec2-instance.tf
+++ 06-Terraform-Datasources/terraform-manifests/c4-ec2-instance.tf
@@ -1,8 +1,8 @@
 # Create EC2 Instance - Amazon Linux
 resource "aws_instance" "my-ec2-vm" {
-  ami           = data.aws_ami.amzlinux.id
+  ami           = data.aws_ami.amzlinux1.id
   instance_type = var.ec2_instance_type
-  key_name      = "terraform-key"
+  key_name      = "AWSS3_Instance_MayurAWS1"
 	user_data = file("apache-install.sh")  
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
--- 06-Terraform-Datasources/terraform-manifests/c6-ami-datasource.tf
+++ 06-Terraform-Datasources/terraform-manifests/c6-ami-datasource.tf
@@ -19,3 +19,27 @@ data "aws_ami" "amzlinux" {
     values = [ "x86_64" ]
   }
 }
+
+
+data "aws_ami" "amzlinux1" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-gp2"]
+  }
+
+  filter {
+     name = "root-device-type"
+    values = [ "ebs" ]
+  }
+  filter {
+    name = "virtualization-type"
+    values = [ "hvm" ]
+  }
+  filter {
+    name = "architecture"
+    values = [ "x86_64" ]
+  }
+}
+
