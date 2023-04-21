418aaf6ca6b7e1d8c8fb019cebb2d065c5ce6dea
Onome Oroh <onomzy2016@gmail.com>
2023-04-21 16:16:31 -0400

    fixed terraform block

--- 03-Terraform-Fundamental-Blocks/03-01-Terraform-Block/terraform-manifests/terraform-block.tf
+++ 03-Terraform-Fundamental-Blocks/03-01-Terraform-Block/terraform-manifests/terraform-block.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
     random = {
       source = "hashicorp/random"
-      version = "3.0.1"
+      version = "3.5.1"
     }    
   }
 }
