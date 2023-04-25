052dd38d00e4ce4f61d51adf9411f0edd56dd8d5
Onome Oroh <onomzy2016@gmail.com>
2023-04-25 14:52:10 -0400

    changes

--- 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
@@ -4,8 +4,8 @@
 resource "aws_instance" "my-ec2-vm" {
   ami               = "ami-03c7d01cf4dedc891"
   instance_type     = "t2.micro"
-  availability_zone = "us-east-1a"
-  #availability_zone = "us-east-1b"
+  #availability_zone = "us-east-1a"
+  availability_zone = "us-east-1b"
   tags = {
     "Name" = "web"
     #"tag1" = "Update-test-1"    
