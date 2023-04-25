070f4bcb3210de0308b3fdb48b13586cb5d06817
Onome Oroh <onomzy2016@gmail.com>
2023-04-25 14:14:22 -0400

    updated AMI

--- 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-01-Resource-Syntax-and-Behavior/terraform-manifests/c2-ec2-instance.tf
@@ -2,7 +2,7 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 
 resource "aws_instance" "my-ec2-vm" {
-  ami               = "ami-047a51fa27710816e"
+  ami               = "ami-03c7d01cf4dedc891"
   instance_type     = "t2.micro"
   availability_zone = "us-east-1a"
   #availability_zone = "us-east-1b"
