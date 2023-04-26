5345fadb09210d4e42f48eee3e841ac09376eafa
Onome Oroh <onomzy2016@gmail.com>
2023-04-26 16:24:12 -0400

    added count

--- 04-Terraform-Resources/04-03-Meta-Argument-count/terraform-manifests/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-03-Meta-Argument-count/terraform-manifests/c2-ec2-instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
   count         = 5
   tags = {
-    "Name" = "web"
-    #"Name" = "web-${count.index}"
+    #"Name" = "web"
+    "Name" = "web-${count.index}"
   }
 }
