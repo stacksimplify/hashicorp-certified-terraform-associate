6f8c30f85f69bf3767d52e4135015274ac31112a
Onome Oroh <onomzy2016@gmail.com>
2023-04-26 16:11:49 -0400

    updated ami

--- 04-Terraform-Resources/04-03-Meta-Argument-count/terraform-manifests/c2-ec2-instance.tf
+++ 04-Terraform-Resources/04-03-Meta-Argument-count/terraform-manifests/c2-ec2-instance.tf
@@ -1,6 +1,6 @@
 # Create EC2 Instance
 resource "aws_instance" "web" {
-  ami           = "ami-047a51fa27710816e" # Amazon Linux
+  ami           = "ami-03c7d01cf4dedc891" # Amazon Linux
   instance_type = "t2.micro"
   count         = 5
   tags = {
