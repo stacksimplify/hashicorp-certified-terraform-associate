5dd30fc9a24b281327d4989f97ed73c565235080
Onome Oroh <onomzy2016@gmail.com>
2023-04-27 20:41:41 -0400

    input variables

--- 05-Terraform-Variables/05-01-Terraform-Input-Variables/v1-Input-Variables-Basic/c4-ec2-instance.tf
+++ 05-Terraform-Variables/05-01-Terraform-Input-Variables/v1-Input-Variables-Basic/c4-ec2-instance.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "my-ec2-vm" {
     sudo yum install httpd -y
     sudo systemctl enable httpd
     sudo systemctl start httpd
-    echo "<h1>Welcome to StackSimplify ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
+    echo "<h1>Welcome to MyDevops Space ! AWS Infra created using Terraform in us-east-1 Region</h1>" > /var/www/html/index.html
     EOF
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
